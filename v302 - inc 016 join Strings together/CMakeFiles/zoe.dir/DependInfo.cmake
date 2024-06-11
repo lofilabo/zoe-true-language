1d50806130c5bb6bdbfa52171873006f3bf35058
lofilabo <richardlofi@gmail.com>
2024-06-11 13:55:31 +0100

    CHANGED STRUCTURE of STORAGE
    moved older vers to ZZZZ-Older-Ones

---
+++ v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/binding.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/binding.c.o"
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/error.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/error.c.o"
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/inet.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/inet.c.o"
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/interpreter.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/interpreter.c.o"
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/lexer.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/lexer.c.o"
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/main.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/main.c.o"
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/parser.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/parser.c.o"
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/tokenizer.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/tokenizer.c.o"
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/unacceptable.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/unacceptable.c.o"
+  "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/unicode.c" "/home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together/CMakeFiles/zoe.dir/unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ v302 - inc 016 join Strings together/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together
+# Build directory: /home/richard/Documents/GitHub/zoe-true-language/v302 - inc 016 join Strings together
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
