0cbe722df2ca68bde8fd86d82a103406ba88aed7
lofilabo <richardlofi@gmail.com>
2024-06-11 20:25:06 +0100

    Changed a few tinies
    added some test in 000-New

---
+++ v402 - Improved and tested/02-cannot-harm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/richard/Documents/GitHub/zoe-true-language/v402 - Improved and tested/02-cannot-harm
+# Build directory: /home/richard/Documents/GitHub/zoe-true-language/v402 - Improved and tested/02-cannot-harm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
