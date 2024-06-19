1c6f013349109658ecc9f063603288feaac8a5f3
lofilabo <richardlofi@gmail.com>
2024-06-19 19:08:09 +0100

    v501 - Language Changes
    Step 1 of refining language

---
+++ v501 - NEW LANGUAGE Changes/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/richard/Documents/03-cannot-harm-NEW-COMMANDS
+# Build directory: /home/richard/Documents/03-cannot-harm-NEW-COMMANDS
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
