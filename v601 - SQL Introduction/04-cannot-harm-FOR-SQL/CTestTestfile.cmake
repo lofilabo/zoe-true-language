24548cbb17136c60b0b767ae690bed0244f9cebc
lofilabo <richardlofi@gmail.com>
2024-06-22 17:14:57 +0100

    changes - now testing database
    testing testing

---
+++ v601 - SQL Introduction/04-cannot-harm-FOR-SQL/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/richard/Documents/04-cannot-harm-FOR-SQL
+# Build directory: /home/richard/Documents/04-cannot-harm-FOR-SQL
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
