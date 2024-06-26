738664b62a6d49bb324b38f93ca591fb43aeb948
lofilabo <richardlofi@gmail.com>
2024-06-26 14:02:15 +0100

    chenged more programming
    new changes are in
    v603 - LANG next changes

---
+++ v603 - LANG next changes/06-cannot-harm-FOR-SQL-New/unacceptable.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int unacceptable_basis(char*);
+
+/* 1 - Changed */
+/*int unacceptable_basis1(int);*/
+/* Accepts a String */
+int unacceptable_basis1(char*);
+
+/* 2 - Changed */
+/* int unacceptable_basis2(int); */ 
+/* Returns a String */
+char* unacceptable_basis2(int);  
+
+
+int unacceptable_basis3(char*);
+int unacceptable_basis4(int);
+
+/*Prototype.  Do Not Modify*/
+int unacceptable_basisZ(int);
+
+int unacceptable_basisZZ1(int,int);
+int unacceptable_basisZZ2(int,int);
+int unacceptable_basisZZ3(int,int);
+
+int unacceptable_basisZZZbase(int,int,int);
+int unacceptable_basisZZZ1(int,int,int);
+int unacceptable_basisZZZ2(int,int,int);
+char* zoe_unacceptable_basisZZZ3(char*,char*,int);//<<<<<<<<<<<<<<<<<<<<<<<< int here
+
+/*char unacceptable_basisZZZ3(int,int,int);*/
+
+
+
+
