fe5092f005df90fec5af6367d51cab4303330198
lofilabo <richardlofi@gmail.com>
2024-06-28 21:47:35 +0100

    end-of-day
    not progress, just beginning to tidy

---
+++ v704-Tidy-Tidy-Again/000-New/check-all-test1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+## echo "Hello $USER"
+## echo "Today is $(date)"
+
+
+echo "No. 001"
+./zoe ./000-New/NNN-TEST-UNACCEPTABLE.zoe
+
+echo "No. 002"
+./zoe ./000-New/TTT-Database-SQL.zoe
+
+echo "No. 003"
+./zoe ./000-New/UUU-Chop-Up-String.zoe
+
+echo "No. 004"
+./zoe ./000-New/UUU-Join-Together-String.zoe
+
+echo "No. 005"
+./zoe ./000-New/VVV-701-LOOP-EXAMLE-1
+
+echo "No. 006"
+./zoe ./000-New/XXX-Demo-Most.zoe
+
+echo "You Finished All Of Them!!Bye for now"
+
+
